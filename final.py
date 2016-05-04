56ed4fde758f1681d6cec333d50b92adffb22fcf
Millun Atluri <millun.atluri@gmail.com>
2016-05-04 19:00:23 -0400

    updated finding the red thingy

--- final.py
+++ final.py
@@ -11,12 +11,12 @@ class Intruder:
 
 #take a image and find the countours of the red cylinder
 def findContours():
-    print "going to take a picture"
+    
+
     setPicSize("small")
     image = takePicture()
     savePicture(image, "image.jpg")
-    print "took picture"
-    show(image)
+
     image = cv2.imread("image.jpg")
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower_red = np.array([0, 80, 80])
@@ -27,8 +27,19 @@ def findContours():
     blurred = cv2.filter2D(edges, -1, np.ones((2, 2), np.float32)/4)
     targets = cv2.findContours(blurred.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
     targets = sorted(targets, key=cv2.contourArea, reverse=True)
-    print targets[0]
-    return targets
+    
+    center = (0,0)
+    if len(targets) == 0:
+        return targets, center
+
+    cnt = targets[0]
+    (x,y), radius = cv2.minEnclosingCircle(cnt)
+    radius = int(radius)
+    center = (int(x), int(y))
+    cv2.circle(image,center,radius,(0,255,0),2)
+    
+    print center
+    return targets, center
 
 
 #cv2.minEnclosingCirle()
@@ -36,9 +47,11 @@ def findContours():
 #have robot rotate and look for intruder
 def search():
     targets = findContours()
-    while(len(targets) == 0):
-        turnBy(20, "deg")
-        targets = findContours()
+    if(len(targets) == 0):
+        turnBy(30, "deg")
+        search()
+
+
 
 #check if infront of a wall
 def checkWall():
